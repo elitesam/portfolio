9f54e1b0ca6425d5bac0f9762b17a24f5171cea7
elitesam <sahusambit6@gmail.com>
2022-06-24 17:35:24 +0530

    minor fixes

--- src/components/Sidebar/Sidebar.component.js
+++ src/components/Sidebar/Sidebar.component.js
@@ -1,7 +1,6 @@
 import { Link, NavLink } from 'react-router-dom'
 import './Sidebar.styles.scss'
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faEnvelope,
@@ -15,7 +14,6 @@ const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
       <img src={LogoS} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="logosubtitle" />
     </Link>
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
